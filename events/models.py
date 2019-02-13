a5155d3786f0a2972cbbe19cfe3aed502b354b6c
PratibhaShrivastav <pratibha.shrivastav99@gmail.com>
2019-02-13 22:05:08 +0530

    events model added

---
+++ events/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+# Create your models here.
+
+class Event(models.Model):
+
+    CATEGORY_OPTIONS = (
+        (0,'Technical'),
+        (1,'Business'),
+        (2,'Product'),
+        (3,'IT'),
+    )
+
+    ACTIVITY_OPTIONS = (
+        (0,'Interested'),
+        (1,'Going'),
+        (2,'Not Interested'),
+    )
+
+    #startup = models.ForeignKey('',related_name='event', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField(auto_now=False)
+    end_date = models.DateField(auto_now=False)
+    category = models.IntegerField(choices=CATEGORY_OPTIONS)
+    activity = models.IntegerField(choices=ACTIVITY_OPTIONS)
+    location = models.TextField(default=None)
+    photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None,null=True,blank=True)
+
+    def __str__(self):
+        return self.title
+    
+
+
+
