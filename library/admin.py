943ad42514ca3a1b5f726116a60b7f807a43a3e4
BadduCoder <sumit7engineer@gmail.com>
2019-02-14 15:20:50 +0530

    Library module added

---
+++ library/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Book, BookLog
+
+admin.site.register(Book)
+admin.site.register(BookLog)
--- rooms/models.py
+++ rooms/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from startups.models import Startup
 
-# Create your models here.
 
 class Room(models.Model):
 
@@ -10,7 +10,20 @@ class Room(models.Model):
         (2,'Conference'),
     )
 
-    type = models.IntegerField(choices=ROOM_CHOICES)
+    room_type = models.IntegerField(choices=ROOM_CHOICES)
     seats = models.IntegerField()
     rent = models.IntegerField()
-    number = models.IntegerField()
+    number = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.number
+
+
+class Seat(models.Model):
+
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    startup = models.ForeignKey(Startup, on_delete=models.CASCADE, null=True, blank=True)
+    is_issued = models.BooleanField(default=False)
+
+    def __str__(self):
+        return 'Seat no. ' + str(self.id)
