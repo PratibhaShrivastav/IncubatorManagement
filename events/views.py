922d5e78a898a08a6415cbec66a21ef75b8aa568
BadduCoder <sumit7engineer@gmail.com>
2019-02-16 18:37:38 +0530

    Event Detail List View added

--- events/views.py
+++ events/views.py
@@ -3,7 +3,8 @@ from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Event
-# Create your views here.
+from django.forms.models import model_to_dict
+
 
 class CreateEvent(LoginRequiredMixin,CreateView):
     model = Event
@@ -16,3 +17,22 @@ class CreateEvent(LoginRequiredMixin,CreateView):
         event.startup = self.request.user.profile.all()[0].startup
         event.save()
         return super().form_valid(form)
+
+def event_all(request):
+    events = Event.objects.all()
+    event_list = []
+    for event in events:
+        event_list.append(model_to_dict(event))
+    
+    context = {'events' : event_list}
+    return render(request,'all_event.html', context=context)
+
+def show_event(request, pk):
+    event = Event.objects.get(id=pk)
+    try:
+        startup = event.startup.all()[0]
+        startup = model_to_dict(startup)
+    except:
+        startup = None
+    event = model_to_dict(event)
+    return render(request, 'show_event.html', context={'event':event,'startup':startup})
